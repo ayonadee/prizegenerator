01cb4e1691d476a77f1e9ab86b1a5a144ae4773a
ayona <ayonaduncan@gmail.com>
2021-04-09 14:43:17 +0000

    pipeline

--- projectwo/deploy_stack.sh
+++ projectwo/deploy_stack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@manager-node:/home/ayona/prizegenerator/projectwo/docker-compose.yaml
+scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@manager-node:docker-compose.yaml
 scp -i ~/.ssh/ansible_id_rsa jenkins@manager-node << EOF
     export DATABASE_URI=${DATABASE_URI}
     export SECRET_KEY=${SECRET_KEY}
-    docker stack deploy --compose-file /home/ayona/prizegenerator/projectwo/docker-compose.yaml projectwo
+    docker stack deploy --compose-file docker-compose.yaml projectwo
 EOF
