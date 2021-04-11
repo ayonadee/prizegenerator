9791b6963e38d859815ccb15ec33f6035d921ba8
ayona <ayonaduncan@gmail.com>
2021-04-11 03:53:32 +0000

    limit on the database query

--- projectwo/coreservice/create.py
+++ projectwo/coreservice/create.py
@@ -5,6 +5,3 @@ from application import models
 db.drop_all()
 db.create_all()
 
-# testuser = models.Users(first_name= "Ayona", last_name= "Duncan", account_number= "", message= "")
-# db.session.add(testuser)
-# db.session.commit()
